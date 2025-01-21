ca9d9eacdafc6820ce20d04a799d736f427a92ec
Viviane <vivianeaguiarc@outlook.com>
2025-01-21 19:06:34 -0300

    feat: add core folder with services and professionals - Created constants for services in services.ts and professionals in professionals.ts - Added core folder containing services and professionals data

---
+++ src/core/professionals/Professional.ts
@@ -0,0 +1,9 @@
+export default interface Professional {
+    id: number;
+    name: string;
+    description: string;
+    avaliation: number;
+    imageURL: string;
+    qtdeAvaliation: number;
+  }
+  
