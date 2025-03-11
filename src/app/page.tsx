8ce9a74a05b685e2de95f1ba1ce51e16ef47b6fb
Viviane <vivianeaguiarc@outlook.com>
2025-03-11 16:30:47 -0300

    fix: duplicate HeaderBar import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Slogan from "@/components/landing/Slogan";
 import OurProfessionals from "@/components/professional/OurProfessionals";
 import OurServices from "@/components/service/OurServices";
 import Footer from "@/components/shared/Footer";
-import HeaderBar from "@/components/shared/HeaderBar";
 import SectionBackground from "@/components/shared/SectionBackground";
 
 
@@ -10,7 +9,6 @@ export default function Home() {
   return (
     <div className="flex flex-col">
       <Slogan />
-      <HeaderBar />
       <SectionBackground image="/banners/servicos.webp">
         <OurServices />
       </SectionBackground>
