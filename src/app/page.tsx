604dd5b2ff892c299789c365ac1531728b614704
viviane <vivianeaguiarc@outlook.com>
2025-03-13 14:40:07 -0300

    feat: created the Section background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import OurProfessionals from "@/components/professional/OurProfessionals";
 import OurServices from "@/components/service/OurServices";
+import SectionBackground from "@/components/shared/SecctionBackground";
 
 export default function Home() {
   return (
     <div className="container mx-auto">
       <h1>Landing-page</h1>
-      <OurServices />
-      <OurProfessionals />
+      <SectionBackground image="/banners/servicos.webp">
+        <OurServices />
+      </SectionBackground>
+       <SectionBackground image="/banners/profissionais.webp">
+        <OurProfessionals />
+      </SectionBackground>
     </div>
   );
 }
