a848415a8907597bbf48d3c5fc071a1620578578
viviane <vivianeaguiarc@outlook.com>
2025-03-13 16:12:39 -0300

    created slogan component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
+import Slogan from "@/components/landing/Slogan";
 import OurProfessionals from "@/components/professional/OurProfessionals";
 import OurServices from "@/components/service/OurServices";
 import Footer from "@/components/shared/Footer";
-import HeaderBar from "@/components/shared/HeaderBar";
 import SectionBackground from "@/components/shared/SecctionBackground";
+
+
 
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <HeaderBar />
+      <Slogan />
       <SectionBackground image="/banners/servicos.webp">
         <OurServices />
       </SectionBackground>
-       <SectionBackground image="/banners/profissionais.webp">
+
+      <SectionBackground image="/banners/profissionais.webp">
         <OurProfessionals />
       </SectionBackground>
-      <Footer />
+    <Footer />
     </div>
   );
 }
